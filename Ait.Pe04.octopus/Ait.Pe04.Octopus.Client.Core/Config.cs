a2938fa93075215f49bff0e91e4bf74c94ede82a
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-06-10 23:07:49 +0200

    Create Config class with make config file method

---
+++ Ait.Pe04.octopus/Ait.Pe04.Octopus.Client.Core/Config.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+
+namespace Ait.Pe04.Octopus.Client.Core
+{
+    public class Config
+    {
+        
+        private static void MakeConfigFile()
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = ds.Tables.Add();
+            DataColumn dc;
+            dc = new DataColumn();
+            dc.ColumnName = "ClientIP";
+            dc.DataType = typeof(string);
+            dt.Columns.Add(dc);
+            dc = new DataColumn();
+            dc.ColumnName = "ServerIP";
+            dc.DataType = typeof(string);
+            dt.Columns.Add(dc);
+            dc = new DataColumn();
+            dc.ColumnName = "Port";
+            dc.DataType = typeof(int);
+            dt.Columns.Add(dc);
+            dc = new DataColumn();
+            dc.ColumnName = "PlaneName";
+            dc.DataType = typeof(string);
+            dt.Columns.Add(dc);
+            DataRow dr = dt.NewRow();
+            dr[0] = "127.0.0.1";
+            dr[1] = "127.0.0.1";
+            dr[2] = 49200;
+            dr[3] = "unknown";
+            dt.Rows.Add(dr);
+            string xmlBestand = Directory.GetCurrentDirectory() + "/config.xml";
+            ds.WriteXml(xmlBestand, XmlWriteMode.WriteSchema);
+        }
+    }
+}
