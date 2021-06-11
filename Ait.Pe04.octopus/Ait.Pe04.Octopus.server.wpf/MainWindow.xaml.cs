86c41f7542bb1a5327117ac3f568222996b00bb3
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-06-12 00:41:15 +0200

    Add methods to Server window

--- Ait.Pe04.octopus/Ait.Pe04.Octopus.server.wpf/MainWindow.xaml.cs
+++ Ait.Pe04.octopus/Ait.Pe04.Octopus.server.wpf/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Ait.Pe04.Octopus.Core.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,148 @@ namespace Ait.Pe04.Octopus.server.wpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        Socket _socket;
+        bool _serverOnline;
+        int _maxConnections = 10;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            StartUpConfig();
+        }
+
+        private void StartUpConfig()
+        {
+            cmbIPs.ItemsSource = IP.GetActiveIPs();
+            for (int port = 49200; port <= 49500; port++)
+            {
+                cmbPorts.Items.Add(port);
+            }
+            ServerConfig.GetConfig(out string savedIP, out int savedPort);
+            try
+            {
+                cmbIPs.SelectedItem = savedIP;
+            }
+            catch
+            {
+                cmbIPs.SelectedItem = "127.0.0.1";
+            }
+            try
+            {
+                cmbPorts.SelectedItem = savedPort;
+            }
+            catch
+            {
+                cmbPorts.SelectedItem = 49200;
+            }
+            btnStartServer.Visibility = Visibility.Visible;
+            btnStopServer.Visibility = Visibility.Hidden;
+        }
+
+        private void SaveConfig()
+        {
+            if (cmbIPs.SelectedItem == null) return;
+            if (cmbPorts.SelectedItem == null) return;
+
+            string ip = cmbIPs.SelectedItem.ToString();
+            int port = int.Parse(cmbPorts.SelectedItem.ToString());
+            ServerConfig.WriteConfig(ip, port);
+        }
+
+        private void StartServer()
+        {
+            _serverOnline = true;
+        }
+
+        private void CloseServer()
+        {
+            _serverOnline = false;
+
+            try
+            {
+                if (_socket != null) _socket.Close();
+            }
+            catch
+            { 
+                
+            }
+            _socket = null;
+        }
+
+        private void StartListening()
+        {
+            IPAddress ip = IPAddress.Parse(cmbIPs.SelectedItem.ToString());
+            int port = int.Parse(cmbPorts.SelectedItem.ToString());
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                _socket.Bind(endPoint);
+                _socket.Listen(_maxConnections);
+                while (_serverOnline)
+                {
+                    if(_socket != null)
+                    {
+                        if (_socket.Poll(100000, SelectMode.SelectRead))
+                        {
+                            HandleClientCall(_socket.Accept());
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void HandleClientCall(Socket clientCall)
+        {
+            byte[] clientRequest = new Byte[1024];
+            string instruction = null;
+
+            while (true)
+            {
+
+                int numByte = clientCall.Receive(clientRequest);
+                instruction += Encoding.ASCII.GetString(clientRequest, 0, numByte);
+                if (instruction.IndexOf("##OVER") > -1)
+                    break;
+            }
+            string serverResponseInText = HandleInstruction(instruction);
+            if (serverResponseInText != "")
+            {
+                byte[] serverResponse = Encoding.ASCII.GetBytes(serverResponseInText);
+                clientCall.Send(serverResponse);
+            }
+            clientCall.Shutdown(SocketShutdown.Both);
+            clientCall.Close();
+        }
+        private string HandleInstruction(string instruction)
+        {
+            return string.Empty;
+        }
+
+        private void cmbIPs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SaveConfig();
+        }
+
+        private void cmbPorts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SaveConfig();
+        }
+
+        private void btnStartServer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnStopServer_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
