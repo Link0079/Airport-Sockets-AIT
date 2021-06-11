3d799c4e782436ee210eca89a9ed794c86d0bdfd
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-06-11 23:06:34 +0200

    Add config and empty button methods to client

--- Ait.Pe04.octopus/Ait.Pe04.octopus.client.wpf/MainWindow.xaml.cs
+++ Ait.Pe04.octopus/Ait.Pe04.octopus.client.wpf/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Ait.Pe04.Octopus.Client.Core.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,196 @@ namespace Ait.Pe04.octopus.client.wpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        Socket _socket;
+        IPEndPoint _serverEndPoint;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            StartUpConfig();
+        }
+
+        private void StartUpConfig()
+        {
+            cmbIPs.ItemsSource = IP.GetActiveIPs();
+            for(int port = 49200; port <= 49500; port++)
+            {
+                cmbPorts.Items.Add(port);
+            }
+            Config.GetConfig(out string planeIP, out string serverIP, out int savedPort, out string planeName);
+
+            try
+            {
+                cmbIPs.SelectedItem = planeIP;
+            }
+            catch
+            {
+                cmbIPs.SelectedItem = "127.0.0.1";
+            }
+            try
+            {
+                cmbPorts.SelectedItem = savedPort;
+            }
+            catch
+            {
+                cmbPorts.SelectedItem = 49200;
+            }
+            txtServerIP.Text = serverIP;
+            txtActivePlane.Text = planeName;
+            btnConnectToServer.Visibility = Visibility.Visible;
+            btnDisconnectFromServer.Visibility = Visibility.Hidden;
+            grpActivePlane.Visibility = Visibility.Hidden;
+        }
+
+        private void ContactServer()
+        {
+            IPAddress serverIP = IPAddress.Parse(txtServerIP.Text);
+            //string activeIP = cmbIPs.SelectedItem.ToString();
+            int serverPort = int.Parse(cmbPorts.SelectedItem.ToString());
+            string activePlane = txtActivePlane.Text;
+
+            _serverEndPoint = new IPEndPoint(serverIP, serverPort);
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string message = "IDENTIFICATION=" + activePlane + "##EOM";
+            lblMyID.Content = SendMessageToServerWaitOnResponse(message);
+        }
+
+        private void btnConnectToServer_Click(object sender, RoutedEventArgs e)
+        {
+            Config.WriteConfig(cmbIPs.SelectedItem.ToString(), txtServerIP.Text, int.Parse(cmbPorts.SelectedItem.ToString()), txtActivePlane.Text);
+            btnConnectToServer.Visibility = Visibility.Hidden;
+            btnDisconnectFromServer.Visibility = Visibility.Visible;
+            grpActivePlane.Visibility = Visibility.Visible;
+
+            cmbIPs.IsEnabled = false;
+            cmbPorts.IsEnabled = false;
+            txtActivePlane.IsEnabled = false;
+            txtServerIP.IsEnabled = false;
+
+            ContactServer();
+        }
+
+        private void btnDisconnectFromServer_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "ID=" + lblMyID.Content + "|BYEBYE##EOM";
+            SendMessageToServerDontWaitOnResponse(message);
+
+            btnConnectToServer.Visibility = Visibility.Visible;
+            btnDisconnectFromServer.Visibility = Visibility.Hidden;
+            grpActivePlane.Visibility = Visibility.Hidden;
+            lblMyID.Content = "";
+
+            cmbIPs.IsEnabled = true;
+            cmbPorts.IsEnabled = true;
+            txtActivePlane.IsEnabled = true;
+            txtServerIP.IsEnabled = true;
+        }
+        private void SendMessageToServerDontWaitOnResponse(string message)
+        {
+            lstOutRequest.Items.Insert(0, message);
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                _socket.Connect(_serverEndPoint);
+                byte[] outMessage = Encoding.ASCII.GetBytes(message);
+                _socket.Send(outMessage);
+                _socket.Shutdown(SocketShutdown.Both);
+                _socket.Close();
+                _socket = null;
+            }
+            catch
+            {
+                btnDisconnectFromServer_Click(null, null);
+            }
+        }
+        private string SendMessageToServerWaitOnResponse(string message)
+        {
+            lstOutRequest.Items.Insert(0, message);
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                _socket.Connect(_serverEndPoint);
+                byte[] outMessage = Encoding.ASCII.GetBytes(message);
+                byte[] inMessage = new byte[8000];
+
+                _socket.Send(outMessage);
+                int responseLength = _socket.Receive(inMessage);
+                string response = Encoding.ASCII.GetString(inMessage, 0, responseLength).ToUpper().Trim();
+                lstInResponse.Items.Insert(0, response);
+                return response;
+
+            }
+            catch
+            {
+                btnDisconnectFromServer_Click(null, null);
+                return "ERROR";
+            }
+            finally
+            {
+                if (_socket != null)
+                {
+                    _socket.Shutdown(SocketShutdown.Both);
+                    _socket.Close();
+                    _socket = null;
+                }
+            }
+
+        }
+
+        private void cmbIPs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //method when IP select is changed
+        }
+
+        private void cmbPorts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //method when Port select is changed
+        }
+
+        private void btnAddPassengers_Click(object sender, RoutedEventArgs e)
+        {
+            //method to add passenger to active plane
+        }
+
+        private void btnSubtractPassengers_Click(object sender, RoutedEventArgs e)
+        {
+            //method to substract passenger from active plane
+        }
+
+        private void btnRequestLane_Click(object sender, RoutedEventArgs e)
+        {
+            //method to request lane for the plane
+        }
+
+        private void btnGoToLane_Click(object sender, RoutedEventArgs e)
+        {
+            //method for the plane to taxi to lane or land on the lane
+        }
+
+        private void btnRequestLiftOff_Click(object sender, RoutedEventArgs e)
+        {
+            //method for plane to request permission for takeoff
+        }
+
+        private void btnRequestLanding_Click(object sender, RoutedEventArgs e)
+        {
+            //method for plane to put request for landing
+        }
+
+        private void btnStartEngine_Click(object sender, RoutedEventArgs e)
+        {
+            //method for plane to start the engine before takeoff
+        }
+
+        private void btnStopEngine_Click(object sender, RoutedEventArgs e)
+        {
+            //method for plane to stop the engine after landing
+        }
+
+        private void btnSOS_Click(object sender, RoutedEventArgs e)
+        {
+            //method for plane to request help in case of emergency
         }
     }
 }
