a33c9ec8b31da4f1c834ec5cd409ab5451bd3459
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-06-12 16:53:49 +0200

    Add ctor and methods to Lane

--- Ait.Pe04.octopus/Ait.Pe04.Octopus.Server.Core/Entities/Lane.cs
+++ Ait.Pe04.octopus/Ait.Pe04.Octopus.Server.Core/Entities/Lane.cs
@@ -8,6 +8,22 @@ namespace Ait.Pe04.Octopus.Core.Entities
     {
         public string Name { get; private set; }
         public bool IsAvailable { get; private set; }
-
+        public Lane(string name)
+        {
+            Name = name;
+            IsAvailable = true;
+        }
+        public bool IsLaneAvailable()
+        {
+            return IsAvailable;
+        }
+        public void LeaveLane()
+        {
+            IsAvailable = true;
+        }
+        public void OccupyLane()
+        {
+            IsAvailable = false;
+        }
     }
 }
