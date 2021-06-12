6e1e7f126d899d2bb4a3dec2d2d02c525435bb31
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-06-12 16:56:25 +0200

    Add Plane service, prop and ctor to project

---
+++ Ait.Pe04.octopus/Ait.Pe04.Octopus.Server.Core/Services/PlaneService.cs
@@ -0,0 +1,16 @@
+﻿using Ait.Pe04.Octopus.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ait.Pe04.Octopus.Core.Services
+{
+    public class PlaneService
+    {
+        public List<Plane> Planes { get; private set; }
+        public PlaneService()
+        {
+            Planes = new List<Plane>();
+        }
+    }
+}
