ea92d4b8236267b0617903bd88cf30e5da09eb57
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-06-12 16:50:46 +0200

    Add Lane entity and props to project

---
+++ Ait.Pe04.octopus/Ait.Pe04.Octopus.Server.Core/Entities/Lane.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ait.Pe04.Octopus.Core.Entities
+{
+    public class Lane
+    {
+        public string Name { get; private set; }
+        public bool IsAvailable { get; private set; }
+
+    }
+}
