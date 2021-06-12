60358072698716f87a6a1aa9361152fe9053219e
Bjorn Vossen <Bjorn.Vossen@student.howest.be>
2021-06-12 16:42:40 +0200

    Add Plane entity and props to project

---
+++ Ait.Pe04.octopus/Ait.Pe04.Octopus.Server.Core/Entities/Plane.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ait.Pe04.Octopus.Core.Entities
+{
+    public class Plane
+    {
+        public string Name { get; private set; }
+        public string Destination { get; private set; }
+        public int TotalPassengers { get; private set; }
+        public int MaxPassengers { get; private set; }
+        public bool IsEngineActive { get; private set; }
+        public bool InFlight { get; private set; }
+        public bool IsOnLane { get; private set; }
+        //public Lane lane { get; private set; }
+    }
+}
